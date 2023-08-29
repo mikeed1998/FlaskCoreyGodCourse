f1644dde356d2ae0e7c8d4ad02a8a88c1418d01e
Michael Eduardo Sandoval Perez <49420226+mikeed1998@users.noreply.github.com>
2023-08-29 15:28:52 -0600

    del tutoral 6

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dc16b97a5546948a8cbfb3dfe414967f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # a donde me manda en caso de fallar login_required
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
 
