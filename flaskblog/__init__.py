e90dd081d01f4d503fb0d5a647ed25951250794f
Michael Eduardo Sandoval Perez <49420226+mikeed1998@users.noreply.github.com>
2023-10-12 16:46:07 -0600

    mejorar parte de los correos

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -12,7 +14,15 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # a donde me manda en caso de fallar login_required
 login_manager.login_message_category = 'info'
-
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_SERVER'] = 'mail.wozial.com'
+app.config['MAIL_USERNAME'] = 'michael@wozial.com'
+app.config['MAIL_PASSWORD'] = 'zCmfxQEz&wTM'
+mail = Mail(app)
 
 from flaskblog import routes
 
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -61,4 +61,25 @@ class PostForm(FlaskForm):
 
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Reset Password')
+
+
+
+
     
